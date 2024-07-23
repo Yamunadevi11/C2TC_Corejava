fe7a540bc21d671e729b900124b39c12a662dd6b
Admin <Admin@DESKTOP-A2USR3M>
2024-07-23 21:21:18 +0530

    "hello"

---
+++ src/csedept/CodingProject.java
@@ -0,0 +1,10 @@
+package csedept;
+
+public class CodingProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module yamunadevi {
+}
